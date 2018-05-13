b12b73852d2728be7e01751e915b069db8e71240
Hugues ALLAIN <hugues.allain@gmail.com>
2018-05-13 11:38:52 +0200

    working all together

--- setup.sh
+++ setup.sh
@@ -1,2 +1,21 @@
+#!/bin/bash
+set -e
+
+OS_INFORMATION="$(uname -s)"
+case "${OS_INFORMATION}" in
+    Linux*)     OS_NAME=linux;;
+    Darwin*)    OS_NAME=mac;;
+    CYGWIN*)    OS_NAME=windows;;
+    MINGW*)     OS_NAME=windows;;
+    *)          OS_NAME="UNKNOWN:${OS_INFORMATION}"
+esac
+
+if [ $OS_NAME == 'linux' ]; then
+    sudo setfacl -dR -m u:$(whoami):rwX -m u:1000:rwX ./
+    sudo setfacl -R -m u:$(whoami):rwX -m u:1000:rwX ./
+elif [ $OS_NAME == 'mac' ]; then
+    sudo dseditgroup -o edit -a $(id -un) -t user $(id -gn 1000)
+fi
+
 docker-compose down
 docker-compose up -d
